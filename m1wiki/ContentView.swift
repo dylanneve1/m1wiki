0c64efdf3b08facac574a56b43f03b53c7bb03cc
Dylan Neve <dylanneve1@gmail.com>
2021-03-12 08:43:41 +0000

    Initial Commit

---
+++ m1wiki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m1wiki
+//
+//  Created by Dylan Neve on 12/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ m1wiki/m1wikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  m1wikiApp.swift
+//  m1wiki
+//
+//  Created by Dylan Neve on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct m1wikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
