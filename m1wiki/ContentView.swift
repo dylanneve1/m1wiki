2b40986f38818fa961a452346bccbeab1403fe44
Dylan Neve <dylanneve1@gmail.com>
2021-03-12 09:05:04 +0000

    Add app icons and update a bit

--- m1wiki/ContentView.swift
+++ m1wiki/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Welcome to Apple M1 Wiki")
+            .fontWeight(.bold)
+            .padding(.all, 20.0)
+            .frame(width: 300.0, height: 50.0, alignment: .top)
+        Text("This is an app where you can find all the useful info you need on Apple's new M1 processors")
+            .padding(.vertical, 5.0)
+            .frame(width: 250.0, height: 75.0, alignment: /*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/)
+        Button("Intel VS Apple Silicon") {
+        }
+        Button("M1 Performance") {
+        }
     }
 }
 
---
+++ m1wiki/buttonListener.swift
@@ -0,0 +1,12 @@
+//
+//  buttonListener.swift
+//  m1wiki
+//
+//  Created by Dylan Neve on 12/03/2021.
+//
+
+import Foundation
+
+// struct buttonListener: {
+    
+// }
